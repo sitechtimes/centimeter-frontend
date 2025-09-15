21162b6f017af532277ff793dfdca33eab1af271
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-09-15 08:03:17 -0700

    THANKS KENNETH NG

--- centimeter/src/views/HomeView.vue
+++ centimeter/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
   <main>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <NavBar />
   </main>
 </template>
