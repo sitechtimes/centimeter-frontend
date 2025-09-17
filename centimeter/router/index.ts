373bd971e8174eaea9dce0f6095afeb1b06787ba
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-09-17 08:02:21 -0700

    WOW KENNETH YOURE SO AWESOME FOR LETTING ME STEAL CODE ❤️

--- centimeter/router/index.ts
+++ centimeter/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/join',
+      name: 'join',
+      component: () => import('../views/JoinView.vue'),
+    }
   ],
 })
 
