c92a37991d22b5a0835a9682e96ff995b295aadb
haobinjhin <haobinz3@nycstudents.net>
2025-10-01 08:06:28 -0700

    sdfsdf

---
+++ centimeter/components/CrePre-Index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <main >
+            <aside class="sidebar h-screen">
+            <div v-for="Component in RightSBComponent" :key="Component.name" class="">
+                <button class="no-underline text-[color:var(--text-color)] font-bold px-4 py-2 rounded hover:bg-[color:var(--primary-light)] transition border-2 w-50 m-2 p-6 ">
+                    {{ Component.name }}
+                </button>
+            </div>
+        </aside>
+    </main>
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+type MakePresentationSideBar = {
+    name: string;
+    owncomponents: []
+}
+/* SB = SideBar */
+const RightSBComponent = [
+    {
+        name: "Edit",
+        owncomponents: []
+    },
+    {
+        name: "Comments",
+        owncomponents: []
+    },
+    {
+        name: "Interactivity",
+        owncomponents: []
+    },
+    {
+        name: "Templates",
+        owncomponents: []
+    },
+
+]as const as MakePresentationSideBar[];
+</script>
+
+<style scoped>
+
+</style>
--- centimeter/src/views/CreatePresentation.vue
+++ centimeter/src/views/CreatePresentation.vue
@@ -1,10 +1,12 @@
 <template>
-    <div>
+    <main>
         <createpresent/>
-    </div>
+    </main>
+    <CreatePresentIndex/>
 </template>
 
 <script setup lang="ts">
 import createpresent from '../../components/CreatePresentBar.vue'
+import CreatePresentIndex from '../../components/CrePre-Index.vue'
 
 </script>
