ac6aad38adc0f2b21293daedd3ee2d3ebd8c5ad4
haobinjhin <haobinz3@nycstudents.net>
2025-10-03 07:23:11 -0700

    sdfsdffsd

--- centimeter/components/CrePre-Index.vue
+++ centimeter/components/CrePre-Index.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <main >
-            <aside class="sidebar h-screen">
+            <aside class="sidebar h-screen p-0">
             <div v-for="Component in RightSBComponent" :key="Component.name" class="">
-                <button class="no-underline text-[color:var(--text-color)] font-bold px-4 py-2 rounded hover:bg-[color:var(--primary-light)] transition border-2 w-50 m-2 p-6 ">
+                <button @click="toggleOwnComponent(Component.o)" class="no-underline text-[color:var(--text-color)] font-bold px-4 py-2 rounded hover:bg-[color:var(--primary-light)] transition border-2 w-50 m-2 p-6 ">
                     {{ Component.name }}
                 </button>
+                
+                <div v-if="owncomponent">
+                    {{ Component.owncomponents }}
+                </div>
+                
             </div>
         </aside>
     </main>
@@ -14,30 +19,40 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 type MakePresentationSideBar = {
     name: string;
-    owncomponents: []
+    owncomponents: unknown[]
 }
+
+const owncomponent = ref(false)
+
 /* SB = SideBar */
 const RightSBComponent = [
     {
         name: "Edit",
-        owncomponents: []
+        owncomponents: ['Edit']
     },
     {
         name: "Comments",
-        owncomponents: []
+        owncomponents: ['Comments']
     },
     {
         name: "Interactivity",
-        owncomponents: []
+        owncomponents: ['Interactivity']
     },
     {
         name: "Templates",
-        owncomponents: []
+        owncomponents: ['Bam']
     },
 
 ]as const as MakePresentationSideBar[];
+
+function toggleOwnComponent(components: []) {
+    owncomponent.value = !owncomponent.value
+    console.log(components)
+}
 </script>
 
 <style scoped>
