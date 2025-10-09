036da666e27e498639819bd6b57d56f4de35b20b
Eric Chen <ericc404@nycstudents.net>
2025-10-09 10:57:21 -0400

    join

--- centimeter/App.vue
+++ centimeter/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import JoinBar from './components/JoinBar.vue';
 </script>
 
 <template>
+  <JoinBar/>
   <header>
   </header>
 
---
+++ centimeter/components/JoinBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="bg-blue-100"
+  >
+    <div class="container mx-auto px-4 py-4">
+      <div class="flex items-center justify-between gap-4">
+        <div class="w-8 flex-shrink-0"></div>
+        
+
+        <div class="flex items-center gap-4 flex-1 justify-center">
+          <span class="text-black font-semibold whitespace-nowrap">
+            Enter code to join a live Menti
+          </span>
+          
+          <div class="flex items-center gap-2">
+            <input
+            placeholder="12345678"
+              type="text"
+              class="px-4 py-2 rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent w-30"
+            />
+            
+            <button
+              class="px-6 py-2 bg-gray-300 text-gray-500  font-semibold rounded-3xl hover:bg-blue-50 transition-colors duration-200 cursor-pointer"
+            >
+              Join
+            </button>
+          </div>
+        </div>
+        
+        <!-- X Button -->
+        <button>
+            <i class="pi pi-times cursor-pointer text-black"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
