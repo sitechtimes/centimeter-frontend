76ae87145d9bf45ed94fa2b748f746638710b43b
Eric Chen <ericc404@nycstudents.net>
2025-10-10 10:25:48 -0400

    vertical shirnk

--- centimeter/components/JoinBar.vue
+++ centimeter/components/JoinBar.vue
@@ -2,10 +2,10 @@
   <div
     class="bg-blue-100"
   >
-    <div class="container mx-auto px-4 py-4">
+    <div class="container mx-auto px-4 py-2">
       <div class="flex items-center justify-between gap-4">
         <div class="w-8 flex-shrink-0"></div>
-        
+
 
         <div class="flex items-center gap-4 flex-1 justify-center">
           <span class="text-black font-semibold whitespace-nowrap">
