2c74a2c755b6cca01e4d30653d89dd1fe1ccf304
Eric Chen <ericc404@nycstudents.net>
2025-10-10 19:38:24 -0400

    JoinBar styles now take colors from main.css for ease of access
    JoinBar isn't used anywhere as of right now

--- centimeter/components/JoinBar.vue
+++ centimeter/components/JoinBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-blue-100"
+    class="bg-[var(--primary-light)]"
   >
     <div class="container mx-auto px-4 py-2">
       <div class="flex items-center justify-between gap-4">
@@ -8,7 +8,7 @@
 
 
         <div class="flex items-center gap-4 flex-1 justify-center">
-          <span class="text-black font-semibold whitespace-nowrap">
+          <span class="text-[var(--text-color)] font-semibold whitespace-nowrap">
             Enter code to join a live Menti
           </span>
           
@@ -16,11 +16,11 @@
             <input
             placeholder="12345678"
               type="text"
-              class="px-4 py-2 rounded-lg bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-white focus:border-transparent w-30"
+              class="bg-[var(--bg-color)] text-[var(--text-color)] px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary)] focus:border-transparent w-30"
             />
             
             <button
-              class="px-6 py-2 bg-gray-300 text-gray-500  font-semibold rounded-3xl hover:bg-blue-50 transition-colors duration-200 cursor-pointer"
+              class="bg-[var(--faded-bg-color)] text-[var(--faded-text-color)] hover:bg-[var(--primary-shade-translucent)] px-6 py-2 font-semibold rounded-3xl transition-colors duration-200 cursor-pointer"
             >
               Join
             </button>
@@ -28,7 +28,7 @@
         </div>
         
         <button>
-            <i class="pi pi-times cursor-pointer text-black"></i>
+            <i class="pi pi-times cursor-pointer text-[var(--text-color)]"></i>
         </button>
       </div>
     </div>
