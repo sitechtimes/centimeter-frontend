14024ca76555ce111469224973f3cf8438998fe9
Eric Chen <ericc404@nycstudents.net>
2025-10-24 10:23:26 -0400

    2424242424ug u poiu 67 767 67 676 7676 767 67 676 767 676 767 67 767667 41 41 414 141 6767676767676
    join bar

--- centimeter/components/Presentation/SideBar.vue
+++ centimeter/components/Presentation/SideBar.vue
@@ -1,9 +1,28 @@
 <template>
-  <li>
-    <i class="text-xl text-black"><FilePenLine /></i>
-  </li>
+  <div class="">
+    <ul class="menu bg-[var(--background-color)] gap-3">
+      <li v-for="item in menuItems" :key="item.id">
+        <i class="text-xl text-black"><component :is="item.icon" /></i>
+        <span class="ml-2 text-black">{{ item.description }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { FilePenLine } from 'lucide-vue-next'
+import {
+  FilePenLine,
+  MessageSquare,
+  Heart,
+  PaintbrushVertical,
+  LayoutTemplate,
+} from 'lucide-vue-next'
+
+const menuItems = [
+  { id: 1, icon: FilePenLine, description: 'Edit' },
+  { id: 2, icon: MessageSquare, description: 'Comments' },
+  { id: 3, icon: Heart, description: 'Interactivity' },
+  { id: 4, icon: PaintbrushVertical, description: 'Themes' },
+  { id: 5, icon: LayoutTemplate, description: 'Templates' },
+]
 </script>
