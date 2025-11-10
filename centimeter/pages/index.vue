d823edf902f411206ec17d2bb337080b75bad4a7
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2025-11-10 07:58:01 -0800

    WHAT THE HELL

---
+++ centimeter/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from '../../components/NavBar.vue';
+import homeindex from '../../components/home-index.vue'
+import JoinBar from '../../components/JoinBar.vue';
+</script>
+
+<template>
+  <main>
+    <NavBar/>
+    <JoinBar/>
+  </main>
+  <homeindex/>
+
+  <div>
+    
+  </div>
+</template>
