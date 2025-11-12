7a462c6cee075ab7b863387466390cacbcddae3a
haobinjhin <haobinz3@nycstudents.net>
2025-11-12 11:02:18 -0500

    dfssdf

--- centimeter/components/UserProfilePic.vue
+++ centimeter/components/UserProfilePic.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <img src="" alt="ProfilePicture" @click="changeProfileDropdown">
-        <h3 v-if="editProfile">
+        <img src="" alt="ProfilePicture" @click="changeAvatarDropdown">
+        <h3 v-if="changeAvatar">
             Change Avatar
         </h3>
-        <h1>{{ email }}</h1>    
+        <h1>{{ UserStore.user?.username }}</h1>
+        <h2>{{ email }}</h2>
     </div>
     <div>
-        <button>Edit Profile</button>
+        <button v-if="!editProfile" @click="editProfileComponent">Edit Profile</button>
+        <div v-if="editProfile">
+            <h1>Username</h1>
+            <input type="text" placeholder="Username" ref="UsernameInput">
+            <br>
+            <button>Save</button>
+            <button @click="cancelProfileChanges">Cancel</button>
+        </div>
     </div>
 </template>
 
@@ -17,10 +25,24 @@ import { ref } from 'vue';
 
 const UserStore = useUserStore()
 const email = ref("")
+const changeAvatar = ref(false)
 const editProfile = ref(false)
 
-const changeProfileDropdown = () => {
+const UsernameInput = ref("")
+
+const changeAvatarDropdown = () => {
+    changeAvatar.value = !changeAvatar.value
+}
+/* pretty sure theres a better name for this */
+const editProfileComponent = () => {
     editProfile.value = !editProfile.value
+}
+const saveProfileChanges = () => {
+    UserStore.user?.username == UsernameInput.value
+    editProfile.value = false
+}
+const cancelProfileChanges = () => {
+    editProfile.value = false
 }
 
 </script>
