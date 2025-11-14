7663b2a92f5be6adb34804fe1d752f56b5c150d5
haobinjhin <haobinz3@nycstudents.net>
2025-11-14 11:03:07 -0500

    dfsdfd

--- centimeter/src/middleware/auth.ts
+++ centimeter/src/middleware/auth.ts
@@ -1,1 +1,8 @@
 import { useUserStore } from "@/stores/user";
+
+export default defineVueRouteMiddleware((to) =>{
+    const userStore = useUserStore()
+
+    if (!to.path.startswith('/auth' || '/'))
+        return navigateTo('/auth/signup')
+})
