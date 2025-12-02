e93c7ba3449b827d8237907f8b8a9e5298abd924
Eric Chen <ericc404@nycstudents.net>
2025-12-02 11:02:31 -0500

    why gap no work...? :frowning:

--- centimeter/components/Dashboard/SideBar.vue
+++ centimeter/components/Dashboard/SideBar.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="fixed left-0">
-        <img src="/logo/logo.svg" class="w-8 h-8" alt="">
-    </div>
+    <nav class="fixed left-0">
+        <div class="flex flex-row ">
+            <img src="/logo/logo.svg" class="w-16 h-16" alt="">
+        </div>  
+        <div class="gap-10">
+            <div class="relative flex flex-col">
+                <div>Home</div>
+                <div>Presentation</div>
+                <div>Shared</div>
+            </div>
+            <div class="relative flex flex-col">
+    
+                <div>Workspace Presentations</div>
+                <div>Templates</div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup lang="ts"></script>
