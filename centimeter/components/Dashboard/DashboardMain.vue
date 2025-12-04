e4604a3115aa015992e92056449bdb8598afa1f7
Eric Chen <ericc404@nycstudents.net>
2025-12-04 11:02:18 -0500

    sdf

---
+++ centimeter/components/Dashboard/DashboardMain.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col flex-none ">
+        <header class="flex flex-col flex-none">
+            <Bell/>
+        </header>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Bell } from 'lucide-vue-next';
+</script>
