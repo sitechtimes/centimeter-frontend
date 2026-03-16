3107180d39ffdb5a7dc031f54a839721fb169d72
Oleg Ponyatyshyn <113192651+olegponyat@users.noreply.github.com>
2026-03-16 10:48:07 -0400

    affafadsa

--- centimeter/utils/types.ts
+++
@@ -1,9 +0,0 @@
-// Re-export all types from organized domain-specific files
-export type { User } from './userTypes'
-export type { Presentation, Slide, SlideComponent } from './presentationTypes'
-export type { JoinSessionResponse, SessionStatus } from './sessionTypes'
-export type { ToastItem, ToastPayload, Option, NavButtons, ShowButtons } from './uiTypes'
-export type { SlidesDropdown, MakePresentationSideBar } from './editorTypes'
-
-// Re-export the presentation code generation utility
-export { generatePresentationCode } from './presentation'
