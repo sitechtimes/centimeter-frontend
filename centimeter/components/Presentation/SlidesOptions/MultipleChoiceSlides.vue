8739cc5cb3e6d9ef8e8a332c517d2110b9adbd10
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2026-03-17 11:02:14 -0400

    sdfsdf

--- centimeter/components/Presentation/SlidesOptions/MultipleChoiceSlides.vue
+++ centimeter/components/Presentation/SlidesOptions/MultipleChoiceSlides.vue
@@ -5,10 +5,10 @@
             aria-label="Presentation canvas"
             class="w-[96vw] max-w-[1400px] aspect-video bg-[whitesmoke] rounded-[6px] shadow-[0_10px_30px_rgba(0,0,0,0.12)] border border-[rgba(0,0,0,0.06)] overflow-hidden p-10"
         >
-            <input id="MultipleChoiceQuestion" type="text" @click="changeText(yourQuestion, 'Ask your question here...', textEditPanel)" v-model="yourQuestion" class="h-[15%] p-5">
-        <div @click="openOptionEditPanel" class="flex flex-wrap gap-4 h-[60%] p-10 border-2 border-transparent hover:border-slate-800 transition-colors duration-200"> 
+            <input id="MultipleChoiceQuestion" type="text" @click="changeText(changedQuestion, 'Ask your question here...', textEditPanel)" v-model="changedQuestion" class="h-[15%] p-5">
+        <div class="flex flex-wrap gap-4 h-[60%] p-10 border-2 border-transparent hover:border-slate-800 transition-colors duration-200"> 
             <div v-for="choice in placeHolderOptions">
-                <ol>{{ choice.position }} + {{ choice.option_text }}</ol>
+                <ol>{{ choice.position }}  {{ choice.option_text }}</ol>
             </div>
 
             <button @click="addOption">add option</button>
@@ -21,31 +21,34 @@
 <script setup lang="ts">
 import type { EditPanel, PollsOption } from '~/utils/types'
 
-const yourQuestion = ref("Ask your question here...")
+const changedQuestion = ref("")
 const textEditPanel = multipleChoiceEditPanel.value[1].open
+const optionEditPanel = multipleChoiceEditPanel.value[2].open
 
 
-const openOptionEditPanel = () => {
-    optionsEditPanel.value = true
-}
 
 const emit = defineEmits({
 
     open: (editPanel: EditPanel) => {
         if (!editPanel.open){
             editPanel.open = true
+            console.log(editPanel)
             return editPanel.open
         }
     },
 
-    text: (text: string, originalText: string, isOpen: boolean) => {
-        if ((text == originalText) && isOpen){
-            text = ""
-            console.log(text, originalText, isOpen)
-            return text
-        } else if ((text !== originalText) && !isOpen) {
-            text = originalText
-            return text
+    text: (changedText: string, defaultText: string, isOpen: boolean) => {
+        if ((changedText == defaultText) && isOpen){
+            changedText = ""
+            console.log(changedText, defaultText, isOpen)
+            return changedText
+        } else if ((changedText == defaultText) && !isOpen) {
+            console.log(changedText, defaultText, isOpen)
+            return changedText
+        } else if (!(changedText == defaultText) && isOpen) {
+            console.log(changedText, defaultText, isOpen)
+        } else {
+            console.log(changedText, defaultText, isOpen)
         }
     }
 
@@ -55,8 +58,8 @@ const openEditPanel = (editPanel: EditPanel) => {
     emit('open', editPanel)
 }
 
-const changeText = (text: string, originalText: string, isOpen: boolean) => {
-    emit('text', text, originalText, isOpen)
+const changeText = (changedText: string, defaultText: string, isOpen: boolean) => {
+    emit('text', changedText, defaultText, isOpen)
 }
 
 const placeHolderOptions = ref<PollsOption[]>([
@@ -86,8 +89,9 @@ const defaultOptionName = computed(()=> <PollsOption>{
 
 
 const addOption = () =>{
-    console.log(textEditPanel)
+    openEditPanel(multipleChoiceEditPanel.value[2])
     placeHolderOptions.value.push(defaultOptionName.value)
+    console.log(optionEditPanel)
 }
 
 </script>
