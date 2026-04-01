3329302c90dcf852e40bef851136c0d0ee2ba12b
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2026-04-01 11:02:21 -0400

    sdfsdfsdfsdfsdff

--- centimeter/components/Presentation/SlidesOptions/MultipleChoiceSlides.vue
+++ centimeter/components/Presentation/SlidesOptions/MultipleChoiceSlides.vue
@@ -1,25 +1,31 @@
 <template>
     <div class="flex-1 flex items-center justify-center p-4 bg-[var(--bg-color)] ">
         <div
+            ref="canvasRef"
             role="region"
             aria-label="Presentation canvas"
+            :style="canvasStyle"
             class="w-[96vw] max-w-[1400px] aspect-video bg-[whitesmoke] rounded-[6px] shadow-[0_10px_30px_rgba(0,0,0,0.12)] border border-[rgba(0,0,0,0.06)] overflow-hidden p-10 hover:border-slate-800 transition-shadow cursor-pointer"
         >
             <input id="MultipleChoiceQuestion" type="text" @click="changeText()" v-model="changedQuestion" class="flex-shrink-0 p-4 text-lg border-b border-gray-200 focus:outline-none focus:border-blue-500">
         <div class="flex flex-wrap gap-4 h-[60%] p-10 border-2 border-transparent"> 
-            <div v-if="multipleChoiceStore.chartType === 'bar'">
-                <Bar :data="multipleChoiceStore.chartData" />
+            <div class="flex flex-col items center gap-4">
+                <div v-if="multipleChoiceStore.chartType === 'bar'">
+                    <Bar :data="multipleChoiceStore.chartData" :options="multipleChoiceStore.chartOptions" />
+                </div>
+                <div v-if="multipleChoiceStore.chartType === 'doughnut'">
+                    <Doughnut :data="multipleChoiceStore.chartData" />
+                </div>
+                <div v-if="multipleChoiceStore.chartType === 'pie'">
+                    <Pie :data="multipleChoiceStore.chartData" />
+                </div>
+                <div class="flex flex-row gap-4">
+                    <div v-for="choice in options" class="flex items-center gap-2 p-3 bg-white rounded border border-gray-200 hover:border-slate-800 transition-colors">
+                        <ol>{{ choice.position }}  {{ choice.option_text }}</ol>
+                    </div>
+                </div>
+                
             </div>
-            <div v-if="multipleChoiceStore.chartType === 'doughnut'">
-                <Doughnut :data="multipleChoiceStore.chartData" />
-            </div>
-            <div v-if="multipleChoiceStore.chartType === 'pie'">
-                <Pie :data="multipleChoiceStore.chartData" />
-            </div>
-            <div v-for="choice in options" class="flex items-center gap-2 p-3 bg-white rounded border border-gray-200 hover:border-slate-800 transition-colors">
-                <ol>{{ choice.position }}  {{ choice.option_text }}</ol>
-            </div>
-
         </div>
     </div>
     </div>
@@ -45,6 +51,11 @@ const changedQuestion = ref("Ask your question here...")
 const textEditPanel = computed(() => multipleChoiceStore.textEditPanel)
 const optionEditPanel = computed(() => multipleChoiceStore.optionEditPanel)
 
+const canvasRef = ref<HTMLDivElement>();
+
+const CANVAS_WIDTH = 1200,
+  CANVAS_HEIGHT = 800;
+const canvasStyle = computed(() => ({ width: `${CANVAS_WIDTH}px`, height: `${CANVAS_HEIGHT}px` }));
 
 const changeText = () => {
     if (changedQuestion.value === "Ask your question here...") {
--- centimeter/stores/slidesStore.ts
+++ centimeter/stores/slidesStore.ts
@@ -11,19 +11,19 @@ export const useMultipleChoiceStore = defineStore( "multipleChoiceStore", () => 
         color: "#27F5EB",
         option_text: "option 1",
         position: 1,
-        amount_chosen: 0
+        amount_chosen: 67
     },
     {
         color: "#F54927" ,
         option_text: "option 2",
         position: 2,
-        amount_chosen: 0
+        amount_chosen: 23
     },
     {
         color: "#000000",
         option_text: "option 3",
         position: 3,
-        amount_chosen: 0
+        amount_chosen: 47
     }
     ])
 
@@ -53,16 +53,36 @@ export const useMultipleChoiceStore = defineStore( "multipleChoiceStore", () => 
     const chartType = ref<'bar' | 'doughnut' | 'pie'>('bar')
 
     const chartData = ref({
-        labels: options.value.map((option: PollsOption) => option.option_text),
-        datasets: [
-            {   
-                label: 'Votes',
-                data: options.value.map((option: PollsOption) => option.amount_chosen),
-                backgroundColor: options.value.map((option: PollsOption) => option.color),
-                borderWidth: 1,
-            }
-        ]
+    labels: options.value.map((option: PollsOption) => option.option_text),
+    datasets: [
+        {
+        label: 'Votes',
+        data: options.value.map((option: PollsOption) => option.amount_chosen),
+        backgroundColor: options.value.map((option: PollsOption) => option.color),
+        borderWidth: 1,
+        }
+    ]
     })
 
-    return { options, addOption, removeOption, textEditPanel, optionEditPanel, multipleChoiceEditPanel, chartType, chartData }
+    const chartOptions = {
+    scales: {
+        y: {
+            ticks: {
+                display: false
+            }
+        },
+        x:{
+            ticks: {
+                display: false
+            }
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+    }
+
+    return { options, addOption, removeOption, textEditPanel, optionEditPanel, multipleChoiceEditPanel, chartType, chartData, chartOptions }
 })
